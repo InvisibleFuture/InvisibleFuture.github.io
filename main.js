3edbf61d96584882498ba73cc5a2d8a230d80cf8
Last <huan0016@gmail.com>
2020-04-20 09:43:47 +0800

    json test

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-// 344
 console.log("wo haha~")
+
+fetch('/main.json', {
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json; charset=utf-8',
+		Authorization: 'SAPISIDHASH 012ao5airhvdu22esdahk3pn74'
+	}
+})
+.then(response => { return response.json();})
+.then(data => { window.console.log(data); })
+.catch(e => {console.log(e)})
